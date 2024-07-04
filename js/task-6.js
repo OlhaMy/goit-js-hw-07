0288011cdb1d4ca711157e2fb0f943ff76078bbf
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-07-04 23:25:49 +0200

    edition task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,12 @@
+const refs = {  
+input: document.querySelector('#controls input'),
+btnCreate: document.querySelector('[data-create]'),
+btnDestroy: document.querySelector('[data-destroy]'),
+boxes: document.querySelector('#boxes'),
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
