1cfb80ccbfc5787fec84a21b357abea08e9f39d7
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-07-05 18:14:51 +0200

    edited HW

--- js/task-6.js
+++ js/task-6.js
@@ -1,23 +1,42 @@
-const refs = {  
-input: document.querySelector('#controls input'),
-btnCreate: document.querySelector('[data-create]'),
-btnDestroy: document.querySelector('[data-destroy]'),
-boxes: document.querySelector('#boxes'),
+const refs = {
+  input: document.querySelector("#controls input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+refs.btnCreate.addEventListener("click", () => {
+  const amount = parseInt(refs.input.value);
+  if (!isNaN(amount) && amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert("Please enter a valid number between 1 and 100");
+  }
+});
+
+refs.btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const boxesFragment = document.createDocumentFragment();
+  const boxSize = 30;
+  const incrementSize = 10;
+
+  for (let i = 0; i < amount; i++) {
+    const size = boxSize + i * incrementSize;
+    const newDiv = document.createElement("div");
+
+    newDiv.style.width = `${size}px`;
+    newDiv.style.height = `${size}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.classList.add("box");
+    boxesFragment.appendChild(newDiv);
+  }
+  refs.boxes.appendChild(boxesFragment);
 }
-
-
-refs.btnCreate.addEventListener('click', createBoxes);
-refs.btnDestroy.addEventListener('click',destroyBoxes);
-
-
-
-
 
 function destroyBoxes() {
-  refs.boxes.innerHTML = '';
+  refs.boxes.innerHTML = "";
 }
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
